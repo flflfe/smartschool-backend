5ecc5553ed7ae7b4a49ec47ad34afbbc96fd52a6
Parajulibkrm <bikramparajuli0000@gmail.com>
2021-07-10 10:39:56 +0545

    Hidden jwt keys in auth file

--- auth/auth.js
+++ auth/auth.js
@@ -1,13 +1,15 @@
 import jsonwebtoken from "jsonwebtoken";
 const { verify } = jsonwebtoken;
 import userModels from "../models/users.js";
+import config from "../config.js";
+const { jwtsigninKey } = config;
 
 const auth = () => async (req, res, next) => {
   try {
     if (!req.header("Authorization")) throw new Error("No Auth Header");
     const token = req.header("Authorization").replace("Bearer ", "");
     console.log(token);
-    const payload = verify(token, "thisisdemokey");
+    const payload = verify(token, jwtsigninKey);
     const user = await userModels.findOne({
       _id: payload._id,
       "tokens.token": token,
